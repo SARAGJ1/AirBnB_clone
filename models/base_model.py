3789c097b10c9ad3d6f2a6ce91a07298d00202c4
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-03-23 13:09:18 +0000

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""This script is the base model"""
+
+
 import datetime
 import uuid
 
 
 class BaseModel:
+    """class that defines all common attributes/methods for other classes"""
 
     def __init__(self, my_number = '', name = ''):
+        """define Public instance attributes"""
+
         self.my_number = my_number
         self.name = name
         self.id = uuid.uuid4()
@@ -14,10 +20,14 @@ class BaseModel:
         self.to_dict()
 
     def save(self):
+        """Update the date of modification"""
+
         self.updated_at = datetime.datetime.now()
         return self.updated_at
 
     def to_dict(self):
+        """create a dic"""
+
         if self.updated_at == None:
             self.updated_at = self.created_at
 
@@ -31,4 +41,6 @@ class BaseModel:
         return self.__dict__
 
     def __str__(self):
+        """return the folowing format"""
+
         return ('[{}] ({}) {}'.format(__class__.__name__, self.id, self.__dict__))
