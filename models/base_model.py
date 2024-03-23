82e870c39755216dd636d68c22408a94f50cc71e
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-03-23 13:20:00 +0000

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,8 @@ class BaseModel:
         """Update the date of modification"""
 
         self.updated_at = datetime.datetime.now()
-        return self.updated_at
+        self.updated_at = self.updated_at.isoformat()
+        self.created_at = self.created_at.isoformat()
 
     def to_dict(self):
         """create a dic"""
